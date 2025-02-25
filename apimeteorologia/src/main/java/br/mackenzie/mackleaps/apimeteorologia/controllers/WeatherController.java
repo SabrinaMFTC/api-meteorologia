13d81f902fa17c35cd5d57afc2095dff215d60c0
AClaver <1001000187@mackenzie.br>
2025-02-25 12:08:45 -0300

    Pequeno Ajuste

--- apimeteorologia/src/main/java/br/mackenzie/mackleaps/apimeteorologia/controllers/WeatherController.java
+++ apimeteorologia/src/main/java/br/mackenzie/mackleaps/apimeteorologia/controllers/WeatherController.java
@@ -13,21 +13,6 @@ import org.springframework.web.bind.annotation.RestController;
 import br.mackenzie.mackleaps.apimeteorologia.dtos.ReportDTO;
 import br.mackenzie.mackleaps.apimeteorologia.services.InfluxServices;
 
-package br.mackenzie.mackleaps.apimeteorologia.controllers;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
-import br.mackenzie.mackleaps.apimeteorologia.dtos.ReportDTO;
-import br.mackenzie.mackleaps.apimeteorologia.services.InfluxServices;
-
 /**
  * Controller responsável por expor os endpoints relacionados aos dados
  * meteorológicos.
